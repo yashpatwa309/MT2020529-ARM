8b4eeca580e23a0b61ad96407afadef7f0c85954
yashpatwa309 <76003452+yashpatwa309@users.noreply.github.com>
2021-08-24 17:20:43 +0530

    Update q1.c

--- assign_one/q1.c
+++ assign_one/q1.c
@@ -1,1 +1,29 @@
+//Find greateest of three numbers
 
+#include<stdio.h>
+
+int main()
+{
+    int a, b, c;
+    printf("Enter three distinct integers : \n");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if(a > b)
+    {
+        if(a > c) 
+            printf("a=%d is the largest of the three numbers", a);
+        else
+            printf("c=%d is the largest of the three numbers", c);
+    }
+ 
+    else
+    {
+        if(b > c) 
+            printf("b=%d is the largest of the three numbers", b);
+        else
+            printf("c=%d is the largest of the three numbers", c);
+    }
+
+return 0;
+
+}
