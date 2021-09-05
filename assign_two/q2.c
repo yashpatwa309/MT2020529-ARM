c3f2a65cbb11d37578ae33faab1a411a5ff97d01
yashpatwa309 <76003452+yashpatwa309@users.noreply.github.com>
2021-09-06 01:34:33 +0530

    Update q2.c

--- assign_two/q2.c
+++ assign_two/q2.c
@@ -1,1 +1,86 @@
+//Empty descending stack
 
+#include <stdio.h>
+
+int SIZE = 10;       
+int head = 9;
+int stack[10];       
+
+int push(int);
+int pop();
+int print(); 
+
+int main()
+{
+   int choice, data;
+   while(1) 
+   {
+     printf("\nEnter your choice: \n 1. PUSH\n 2. POP\n 3. PRINT\n");
+     scanf("%d", &choice);
+
+     switch(choice)
+     {
+        case 1:
+        	printf("\nEnter the element you wish to add to the stack \n");
+        	scanf(" %d", &data);
+        	push(data);
+        	break;
+     
+        case 2: 
+		      pop();
+		      break;
+
+	      case 3: 
+		      print();
+		      break;
+
+	      default:
+		      printf("\n Wrong choice!!");
+		      break;
+     }
+
+    return 0;
+}
+
+
+int push(int data) 
+{
+
+   if(!(head == -1)) 
+   {
+      stack[head] = data;      
+      head = head - 1;   
+      
+   } 
+   else 
+   {
+      printf("Stack is full\n");
+   }
+}            
+
+int pop() 
+{
+
+   int data;
+	
+   if(!(head == SIZE - 1)) 
+   {
+      data = stack[head + 1];
+      head = head + 1;   
+      printf("Element removed : %d \n", data);
+   } 
+   else
+   {
+      printf("Stack is empty\n");
+   }
+
+}
+
+int print()
+{
+   int i;
+   printf("Element at the head of the stack: %d\n" ,stack[head]);
+   printf("Elements: \n"); 
+   for(i=SIZE-1; i>=head; i--)
+      printf("%d\n",stack[i]);
+}
